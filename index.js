ec3b3de7da3c43880e628d1009c16392ae5a1067
Dmitry Ovsienko <ovsienkodima@mail.ru>
2022-08-29 22:00:03 +0300

    added duration setting

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
-  const trick = 'impossible'
+  const trick = 'impossible' //написать названию анимации из списка ниже
+
+  const duration = 4 //длительность анимации
 
   const skateboard = document.querySelector('.skateboard')
 
-  skateboard.style.animation = `${trick} 4s linear infinite`
+  skateboard.style.animation = `${trick} ${duration}s linear infinite`
 
 });
 
