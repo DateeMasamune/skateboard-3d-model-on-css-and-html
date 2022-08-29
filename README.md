51355faa39dfb92f215732e49d15881a5ca26100
Dmitry Ovsienko <ovsienkodima@mail.ru>
2022-08-29 21:55:15 +0300

    added js file for change animation

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ### made with css html<br>
 *** 
 ### just open index.html in browser
+***
+### to change the animation, you need to change the variable with name "trick" in index.js to the name from the list of tricks
---
+++ index.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+
+  const trick = 'impossible'
+
+  const skateboard = document.querySelector('.skateboard')
+
+  skateboard.style.animation = `${trick} 4s linear infinite`
+
+});
+
+/**
+ * TRICK LIST
+ * 
+ * flip
+ * impossible
+ */
